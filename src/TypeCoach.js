4124310eda7851ad9dedc1a7cdc29fa41f27eda1
Wouter Pieter Stekelenburg <woupiestek@yahoo.com>
2024-05-09 14:44:08 +0200

    Accumulate errors

--- src/TypeCoach.js
+++ src/TypeCoach.js
@@ -4,9 +4,38 @@ import { Heap } from "./Heap";
 const testSet =
   "qwertyuiop[]QWERTYUIOP{}asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?";
 
+class RunningMedian {
+  #lower = new Heap();
+  #higher = new Heap();
+  #balance() {
+    return this.#lower.size() - this.#higher.size();
+  }
+  add(value) {
+    if (value < this.#higher.head()) {
+      this.#lower.add(value, value);
+      while (this.#balance() > 1) {
+        const v = this.#lower.pop();
+        this.#higher.add(-v, v);
+      }
+    } else {
+      this.#higher.add(-value, value);
+      while (this.#balance() < 0) {
+        const v = this.#higher.pop();
+        this.#lower.add(v, v);
+      }
+    }
+  }
+  get() {
+    if (this.#balance() > 0) {
+      return this.#lower.head();
+    }
+    return (this.#lower.head() + this.#higher.head()) / 2;
+  }
+}
+
 export class TypeCoach extends LitElement {
   static properties = {
-    code: { type: Number },
+    current: { type: Object },
     median: { type: Number },
   };
 
@@ -20,7 +49,6 @@ export class TypeCoach extends LitElement {
       .main {
         cursor: pointer;
       }
-
       .main:focus {
         background: #ffddff;
       }
@@ -29,55 +57,60 @@ export class TypeCoach extends LitElement {
 
   constructor() {
     super();
-    this.delta = 0;
+    this.median = 0;
+    this.__items = [];
     this.__codes = new Heap();
     for (let i = 0, l = testSet.length; i < l; i++) {
-      this.__codes.add(Math.random(), testSet.charCodeAt(i));
+      this.__items[i] = {
+        code: testSet.charCodeAt(i),
+        score: 0,
+      };
+      this.__codes.add(Math.random() * .1, this.__items[i]);
     }
-    this.code = this.__codes.head();
-
+    this.current = this.__codes.head();
     this.__timeStamp = 0;
-    this.__faster = new Heap();
-    this.__slower = new Heap();
+    this.__median = new RunningMedian();
+    this.__hardCaseThreshold = 0;
   }
 
   #onKey(e) {
-    if (this.__codes.head() === e.charCode) {
-      this.__codes.updatePrio(-(2 ** -this.errors));
-      this.code = this.__codes.head();
-      this.__errors = 0;
-
-      // delta time
-      const delta = e.timeStamp - this.__timeStamp;
+    if (this.current.code === e.charCode) {
+      this.current.score++;
+      this.__codes.updatePrio(-(2 ** this.current.score));
+      this.current = this.__codes.head();
+      // store correct presses per minute
+      this.__median.add(12e4 / (e.timeStamp - this.__timeStamp));
       this.__timeStamp = e.timeStamp;
+      this.median = this.__median.get();
 
-      console.log(6e4 / delta);
-
-      // compute new median
-      if (delta > this.median) {
-        this.__slower.add(-delta, delta);
-        if (this.__slower.size() > this.__faster.size()) {
-          const x = this.__slower.pop();
-          this.__faster.add(x, x);
-        }
-      } else {
-        this.__faster.add(delta, delta);
-        if (this.__faster.size() > 1 + this.__slower.size()) {
-          const x = this.__faster.pop();
-          this.__slower.add(-x, x);
-        }
+      // there is a smarter way
+      if (this.__items.every((it) => it.score > this.__hardCaseThreshold)) {
+        this.__hardCaseThreshold++;
       }
-      this.median = 12e4 / (this.__slower.head() + this.__faster.head());
     } else {
-      this.__errors += 1;
+      this.current.score--;
     }
   }
 
   render() {
-    return html`<div class="main font" tabindex="0" @keypress="${this.#onKey}">
-        ${String.fromCharCode(this.code)}
-      </div>
-      <div class="font">${this.median} p/m</div>`;
+    return html`
+      <h1>
+        Next symbol:
+        <span class="main" tabindex="0" @keypress="${this.#onKey}">
+          ${String.fromCharCode(this.current.code)}
+  </span>
+      </h1>
+      Around ${this.median} successes per minute.
+      <h1>Level ${this.__hardCaseThreshold} hard cases</h1>
+      ${
+      this.__items.map(
+        (it) =>
+          html`<span ?hidden="${it.score > this.__hardCaseThreshold}"
+            >${String.fromCharCode(it.code)}</span
+          >`,
+      )
+    }
+    `;
   }
 }
 
