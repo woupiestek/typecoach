d096cb5e47c8439f68de5021f85e59fdbb33292f
Wouter Pieter Stekelenburg <woupiestek@yahoo.com>
2025-03-27 17:53:49 +0100

    Change to majority of new test texts

--- src/TypeCoach3.js
+++ src/TypeCoach3.js
@@ -91,14 +91,14 @@ export class TypeCoach extends LitElement {
   static #BEEP = new Beep();
 
   // current batch of text strings
-  #input = [];
+  #input = [generate()];
   // next batch of test strings
   #output = [];
 
   #generate() {
     if (!this.#input.length) {
       this.#input = this.#output;
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0, l = 1 + this.#input.length; i < l; i++) {
         this.#input.push(generate());
       }
       shuffle(this.#input);
